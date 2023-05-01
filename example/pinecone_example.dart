8f15cfb310d9d460164e3ad0f41b476ee1edcbcb
Wahab Alshahin <wahab.alshahin@gmail.com>
2023-05-01 15:09:50 -0400

    Initial commit

---
+++ example/pinecone_example.dart
@@ -0,0 +1,6 @@
+import 'package:pinecone/pinecone.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/pinecone.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library pinecone;
+
+export 'src/pinecone_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
