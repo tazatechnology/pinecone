a608cec1fd8e3be75e3f4407c7212af51f0b6101
David Miguel <me@davidmiguel.com>
2023-10-10 08:29:49 +0200

    Fix static analysis warning in generated files

--- lib/src/generated/client.dart
+++ lib/src/generated/client.dart
@@ -1,6 +1,7 @@
 // coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // ignore_for_file: type=lint
+// ignore_for_file: invalid_annotation_target
 
 import 'dart:io' as io;
 import 'dart:convert';
--- lib/src/generated/schema/schema.dart
+++ lib/src/generated/schema/schema.dart
@@ -1,6 +1,7 @@
 // coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // ignore_for_file: type=lint
+// ignore_for_file: invalid_annotation_target
 
 library pinecone_schema;
 
